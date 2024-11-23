d83bb3127b353e88126d859f00ffd338574e7e42
Venil <venilv912@gmail.com>
2024-11-24 00:41:06 +0530

    added a password security and email validity

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,9 +2,22 @@ import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
 import jwt from 'jsonwebtoken';
+import { validateEmail, validatePassword } from "../utils/validation.js";
 
 export const signup = async (req, res, next) => {
     const {username, email, password} = req.body;
+
+    const validUser = await User.findOne({username});
+    if (validUser) return next(errorHandler(409, 'Username already Exists!'));
+
+    if (!validateEmail(email)) return next(errorHandler(400, 'Invalid Email Format'));
+
+    const validEmail = await User.findOne({email});
+    if (validEmail) return next(errorHandler(409, 'Email already Exists!'));
+    
+    const passwordError = validatePassword(password);
+    if (passwordError) return next(errorHandler(400, passwordError));
+
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({username, email, password: hashedPassword});
     try{
@@ -19,42 +32,14 @@ export const signin = async (req, res, next) => {
     const {email, password} = req.body;
     try{
         const validUser = await User.findOne({email});
-        if (!validUser) return next(errorHandler(404, 'User Not Found!'));
+        if (!validUser) return next(errorHandler(401, 'Invalid Username or Password'));
         const validPassword = bcryptjs.compareSync(password, validUser.password);
-        if (!validPassword) return next(errorHandler(401, 'Wrong Credentials!'));
+        if (!validPassword) return next(errorHandler(401, 'Invalid Username or Password!'));
         const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
         const {password: pass, ...rest} = validUser._doc;
         res.cookie('access_token', token, {httpOnly: true}).status(200).json(rest);
     } catch(error)
     {
-        next(error);
-    }
-};
-
-export const google = async (req, res, next) => {
-    try{
-        const user = await User.findOne({email: req.body.email});
-        if (user) {
-            if (req.body.type === 1) return next(errorHandler(409, 'Account Already Exists!'));
-            const token = jwt.sign({id: user._id}, process.env.JWT_SECRET);
-            const {password: pass, ...rest} = user._doc;
-            res.cookie('access_token', token, {httpOnly: true}).status(200).json(rest);
-        }
-        else {
-            if (req.body.type === 2) return next(errorHandler(404, 'Account Not Found!'));
-            const generatedPassword = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
-            const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
-            const newUser = new User({username: req.body.name.split(" ").join("").toLowerCase()+Math.random().toString(36).slice(-4), email: req.body.email, password: hashedPassword, avatar: req.body.photo});
-            try{
-                await newUser.save();
-                const token = jwt.sign({id: newUser._id}, process.env.JWT_SECRET);
-                const {password: pass, ...rest} = await newUser._doc;
-                res.cookie('access_token', token, {httpOnly: true}).status(200).json(rest);
-            }catch(error){
-                next(error);
-            }
-        }
-    } catch(error) {
         next(error);
     }
 };
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -3,8 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { signInStart, signInSuccess, signInFailure, clearError } from '../redux/user/userSlice.js';
 import './Login.css';
-import OAuthsignup from '../components/OAuthsignup.jsx';
-import OAuthsignin from '../components/OAuthsignin.jsx';
+
 export default function Login() {
   const [isActive, setIsActive] = useState(false);
   const [formData, setFormData] = useState({});
@@ -33,7 +32,7 @@ export default function Login() {
             });
             const data = await res.json();
             if (data.success === false) {
-                setError('Username or Email already Exists!');
+                setError(data.message);
                 setLoading(false);
                 return;
             }
@@ -109,7 +108,6 @@ export default function Login() {
           <input type="text" placeholder="Email" required id='email' onChange={handleChange}/>
           <input type="password" placeholder="Password" required id='password' onChange={handleChange}/>
           <button disabled={loading} style={{width: 280, fontSize: 14,}}>{loading ? 'Loading...' : 'Sign Up'}</button>
-          <OAuthsignup />
         </form>
       </div>
 
@@ -122,7 +120,6 @@ export default function Login() {
           <input type="text" placeholder="Email" required id='email2' onChange={handleChange}/>
           <input type="password" placeholder="Password" required id='password2' onChange={handleChange}/>
           <button disabled={Loading} style={{width: 280, fontSize: 14,}}>{loading ? 'Loading...' : 'Sign In'}</button>
-          <OAuthsignin />
         </form>
       </div>
 
