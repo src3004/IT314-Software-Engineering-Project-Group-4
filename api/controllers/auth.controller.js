b726bc023010a93284d883034f58ac6706f303f9
Ghost <chintub2507@gmail.com>
2024-11-30 15:14:13 +0530

    Update auth.controller.js

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -32,9 +32,9 @@ export const signin = async (req, res, next) => {
     const {email, password} = req.body;
     try{
         const validUser = await User.findOne({email});
-        if (!validUser) return next(errorHandler(401, 'Invalid Username or Password'));
+        if (!validUser) return next(errorHandler(401, 'Invalid E-mail or Password'));
         const validPassword = bcryptjs.compareSync(password, validUser.password);
-        if (!validPassword) return next(errorHandler(401, 'Invalid Username or Password!'));
+        if (!validPassword) return next(errorHandler(401, 'Invalid E-mail or Password!'));
         const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
         const {password: pass, ...rest} = validUser._doc;
         res.cookie('access_token', token, {httpOnly: true}).status(200).json(rest);
