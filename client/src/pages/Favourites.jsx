4358e008345253c5fcba335521760142c6e47f46
venilv912 <venilv912@gmail.com>
2024-12-01 06:04:57 +0530

    complete my favourites page functionality

--- client/src/pages/Favourites.jsx
+++ client/src/pages/Favourites.jsx
@@ -1,76 +1,94 @@
 import React from "react";
 import "./Favourites.css";
-
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import bedroomIcon from '../assets/furnished-icon.png';
 import parkingIcon from '../assets/parking-icon.png';
 import noParkingIcon from '../assets/noparking-icon.png';
 import locationIcon from '../assets/location-icon.png';
 
-const Favourites = () => {
-  const properties = [
-    {
-      name: "Modern Family Home",
-      price: "500000",
-      location: "Gandhinagar",
-      bedrooms: 3,
-      parking: false,
-    },
-    {
-      name: "Parshwa Luxuria",
-      price: "700000",
-      location: "Rajkot",
-      bedrooms: 2,
-      parking: false,
-    },
-    {
-      name: "Maitri Elevate",
-      price: "600000",
-      location: "Ahmedabad",
-      bedrooms: 1,
-      parking: true,
-    },
-    {
-      name: "Aadhya Arambh",
-      price: "550000",
-      location: "Anand",
-      bedrooms: 3,
-      parking: true,
-    },
-    {
-      name: "Venus Deshna",
-      price: "600000",
-      location: "Surat",
-      bedrooms: 3,
-      parking: false,
-    },
-    {
-      name: "The Sovereign",
-      price: "575000",
-      location: "Jamnagar",
-      bedrooms: 4,
-      parking: true,
-    },
-  ];
+export default function Favourites()
+{
+  const { currentUser } = useSelector((state) => state.user);
+  const [userFav, setUserFav] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [deleteError, setDeleteError] = useState(false);
+  const [deleteLoading, setDeleteLoading] = useState(false);
 
+  useEffect(() => {
+    const fetchFav = async () => {
+      setLoading(true);
+      setError(false);
+      try {
+        const res = await fetch(`/api/favourite/get/${currentUser._id}`);
+        const data = await res.json();
+        if (data.success === false) {
+          setError(true);
+          return;
+        }
+        setError(false);
+        setUserFav(data);
+      } catch (err) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchFav();
+  }, [currentUser]);
+
+  const handleFavDelete = async (listingId) => {
+    try {
+      setDeleteLoading(true);
+      setDeleteError(false);
+      const res = await fetch(`/api/favourite/delete/${listingId}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (data.success === false)
+      {
+        setDeleteError(data.message);
+        setDeleteLoading(false);
+        return;
+      }
+      setDeleteLoading(false);
+      setUserFav((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+      setDeleteError(error);
+      setDeleteLoading(false);
+    }
+  };
+
+  const properties = userFav;
   return (
     <div className="Saved_Property">
       <h2 className="font-semibold text-2xl text-center mb-4" style={{color: '#2d9c2d'}}>Favourites</h2>
-      <div className="properties-grid">
-        {properties.map((property, index) => (
+      {loading && <p className='text-center my-7 text-2xl font-semibold text-gray-700'>Loading...</p>}
+      {error && <p className='text-center my-7 text-2xl font-semibold text-gray-700'>Error showing Favourites!</p>}
+      {properties && properties.length === 0 && (
+        <div className='text-gray-700 text-center font-semibold' style={{padding: 225}}>No Favourite Properties!</div>
+      )}
+      {deleteError && <p className='text-center text-red-700 mb-3'>{deleteError}</p>}
+      {properties && !loading && !error && (
+        <div className="properties-grid">
+        {properties.slice().reverse().map((property, index) => (
           <div key={index} className="property-card">
-            <div
+            <Link to={`/listing/${property.listingId}`}><div
               className="property-image"
               style={{
                 backgroundImage: `url(${property.image})`,
               }}
-            ></div>
+              ></div></Link>
             <h3>{property.name}</h3>
             <div className='flex justify-center gap-12'>
               <p className="price">₹ {property.price}</p>
               <p className='price'>For {((property.type)=='sale'?'Sale':'Rent')}</p>
             </div>
-            {/* <p className="price">{property.price}</p> */}
             <div className="location details">
               <span>
                 <img
@@ -78,7 +96,7 @@ const Favourites = () => {
                   alt="Location"
                   className="icon"
                 />
-                {property.location}
+                {property.city}
               </span>
             </div>
             <div className="details">
@@ -87,7 +105,7 @@ const Favourites = () => {
                   src={bedroomIcon}
                   alt="Bedroom"
                   className="icon bedroom"
-                />
+                  />
                 {property.bedrooms}
               </span>
               <span>
@@ -95,15 +113,14 @@ const Favourites = () => {
                   src={property.parking ? parkingIcon : noParkingIcon}
                   alt="Parking"
                   className="icon"
-                />
+                  />
               </span>
             </div>
-            <button className="remove-btn">Remove</button>
+            <button disabled={deleteLoading} className="remove-btn" onClick={() => handleFavDelete(property._id)}> Remove</button>
           </div>
         ))}
       </div>
+      )}
     </div>
   );
 };
-
-export default Favourites;
